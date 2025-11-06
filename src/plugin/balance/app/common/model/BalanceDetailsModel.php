4468e6edf41774b802cf08d9c036abf027b0f979
邹意 <741599086@qq.com>
2025-11-06 16:37:38 +0800

    修复附件字段错误

--- src/plugin/balance/app/common/model/BalanceDetailsModel.php
+++ src/plugin/balance/app/common/model/BalanceDetailsModel.php
@@ -27,7 +27,7 @@ class BalanceDetailsModel extends BaseModel
                 'change_value'   => 'float',
                 'change_balance' => 'float',
             ],
-            'file'               => [ // 包含附件的字段，''代表直接等于附件路劲，'array'代表数组中包含附件路劲，'editor'代表富文本中包含附件路劲
+            'fileField'          => [ // 包含附件的字段，''代表直接等于附件路劲，'array'代表数组中包含附件路劲，'editor'代表富文本中包含附件路劲
             ],
         ];
     }
